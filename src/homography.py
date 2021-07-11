7653fd90e95747c9de3e4b28b0a8625dc99eaa2c
tida <tdamuda@gmail.com>
2021-07-11 10:33:01 +0900

    [add] docstring, コメント

--- src/homography.py
+++ src/homography.py
@@ -7,10 +7,21 @@ import numpy as np
 def calc_min_max_coordinate(
     image: np.ndarray, homography: np.ndarray
 ) -> Tuple[int, int, int, int]:
+    """
+    移動先座標の最小最大を計算
+
+    Args:
+        image (np.ndarray): 画像
+        homography (np.ndarray): imageのホモグラフィー
+
+    Returns:
+        min_x, min_y, max_x, max_y (int): 最小最大座標
+    """
     height, weight, _ = image.shape
     min_x, min_y = np.inf, np.inf
     max_x, max_y = 0, 0
 
+    # 射影変換では四隅が最小最大になることが保証されるはず
     corners = np.array([[0, 0, 1], [0, height, 1], [weight, 0, 1], [weight, height, 1]])
     for corner in corners:
         warped_corner = homography.reshape(3, 3) @ corner
@@ -29,6 +40,18 @@ def calc_min_max_coordinate(
 def calc_image_size(
     images: List[np.ndarray], homographies: np.ndarray
 ) -> Tuple[int, int, Tuple[int, int]]:
+    """
+    複数の画像をStitchingした際の画像サイズを計算
+
+    Args:
+        images (np.ndarray): 画像の集合
+        homographies (np.ndarray): ホモグラフィーの集合
+
+    Returns:
+        width (int), height(int), shift (int, int):
+            すべての画像の最小座標と最大座標の差
+            shiftは最も小さい座標値の符号を反転させたもの
+    """
     min_x, min_y = 0, 0
     max_y, max_x, _ = images[0].shape
 
@@ -44,17 +67,29 @@ def calc_image_size(
 
     width = max_x - min_x
     height = max_y - min_y
-    shift = (-min_x, -min_y)
+    shift = (-min_x, -min_y) # 初期値が0のため0か正になる
 
     return width, height, shift
 
 
 def composite_homographies(homographies: np.ndarray) -> np.ndarray:
+    """
+    ベース画像に合わせるためにホモグラフィーを合成
+    H(n_comp) = H(1) * H(2) * ... * H(n)
+
+    Args:
+        homographies (np.ndarray): ホモグラフィーの集合
+
+    Returns:
+        合成されたホモグラフィー
+    """
     homographies_reverse = homographies[::-1]
     composited_homographies = np.empty((0, 9))
 
     for i, composited_homography in enumerate(homographies):
         composited_homography = composited_homography.reshape((3, 3))
+
+        # 逆順にしているため、自分より後のもののみ合成
         for homography in homographies_reverse[len(homographies) - i :]:
             homography = homography.reshape((3, 3))
             composited_homography = homography @ composited_homography
