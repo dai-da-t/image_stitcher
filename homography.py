711eb5c8e6a3578db8e35c2e64bb9f177638cde9
tida <tdamuda@gmail.com>
2021-07-11 09:40:06 +0900

    [clean] format by black

--- homography.py
+++ homography.py
@@ -3,7 +3,9 @@ from typing import List, Tuple
 import numpy as np
 
 
-def calc_min_max_coordinate(image: np.ndarray, homography: np.ndarray) -> Tuple[int, int, int, int]:
+def calc_min_max_coordinate(
+    image: np.ndarray, homography: np.ndarray
+) -> Tuple[int, int, int, int]:
     height, weight, _ = image.shape
     min_x, min_y = np.inf, np.inf
     max_x, max_y = 0, 0
@@ -23,12 +25,16 @@ def calc_min_max_coordinate(image: np.ndarray, homography: np.ndarray) -> Tuple[
     return min_x, min_y, max_x, max_y
 
 
-def calc_image_size(images: List[np.ndarray], homographies: np.ndarray) -> Tuple[int, int, Tuple[int, int]]:
+def calc_image_size(
+    images: List[np.ndarray], homographies: np.ndarray
+) -> Tuple[int, int, Tuple[int, int]]:
     min_x, min_y = 0, 0
     max_y, max_x, _ = images[0].shape
 
     for image, homography in zip(images[1:], homographies):
-        min_x_tmp, min_y_tmp, max_x_tmp, max_y_tmp = calc_min_max_coordinate(image, homography)
+        min_x_tmp, min_y_tmp, max_x_tmp, max_y_tmp = calc_min_max_coordinate(
+            image, homography
+        )
 
         min_x = min(min_x_tmp, min_x)
         min_y = min(min_y_tmp, min_y)
@@ -48,12 +54,14 @@ def composite_homographies(homographies: np.ndarray) -> np.ndarray:
 
     for i, composited_homography in enumerate(homographies):
         composited_homography = composited_homography.reshape((3, 3))
-        for homography in homographies_reverse[len(homographies) - i:]:
+        for homography in homographies_reverse[len(homographies) - i :]:
             homography = homography.reshape((3, 3))
             composited_homography = homography @ composited_homography
 
         composited_homography = composited_homography.reshape(1, 9)
 
-        composited_homographies = np.vstack((composited_homographies, composited_homography))
+        composited_homographies = np.vstack(
+            (composited_homographies, composited_homography)
+        )
 
     return composited_homographies
--- matching.py
+++ matching.py
@@ -18,7 +18,9 @@ def load_images(image_paths: List[str]) -> List[np.ndarray]:
     return images
 
 
-def detect_keypoints(images: List[np.ndarray]) -> Tuple[List[List[cv2.KeyPoint]], List[np.ndarray]]:
+def detect_keypoints(
+    images: List[np.ndarray],
+) -> Tuple[List[List[cv2.KeyPoint]], List[np.ndarray]]:
     akaze = cv2.AKAZE_create()
     keypoints: List[List[cv2.KeyPoint]] = []
     desctiptors: List[np.ndarray] = []
@@ -31,7 +33,12 @@ def detect_keypoints(images: List[np.ndarray]) -> Tuple[List[List[cv2.KeyPoint]]
     return keypoints, desctiptors
 
 
-def match_keypoints(descriptors1: np.ndarray, descriptors2: np.ndarray, distance_threthold: float, ratio_threthold: float) -> Dict[Tuple[int, int], float]:
+def match_keypoints(
+    descriptors1: np.ndarray,
+    descriptors2: np.ndarray,
+    distance_threthold: float,
+    ratio_threthold: float,
+) -> Dict[Tuple[int, int], float]:
     descriptors1 = descriptors1.astype(np.int64)
     descriptors2 = descriptors2.astype(np.int64)
     matches = {}
@@ -43,25 +50,36 @@ def match_keypoints(descriptors1: np.ndarray, descriptors2: np.ndarray, distance
         first_distance = distances[near_indices[0]]
         second_distance = distances[near_indices[1]]
 
-        if (distance_threthold <= first_distance or
-            ratio_threthold <= first_distance / second_distance):
+        if (
+            distance_threthold <= first_distance
+            or ratio_threthold <= first_distance / second_distance
+        ):
             continue
 
         matches[(i, near_indices[0])] = first_distance
     return matches
 
 
-def match_all_images(descriptors: List[np.ndarray], distance_threthold: float, ratio_threthold: float, cross_check: bool = False) -> List[Dict[Tuple[int, int], float]]:
+def match_all_images(
+    descriptors: List[np.ndarray],
+    distance_threthold: float,
+    ratio_threthold: float,
+    cross_check: bool = False,
+) -> List[Dict[Tuple[int, int], float]]:
     all_matches: List[Dict[Tuple[int, int], float]] = []
 
     for i, descriptor in enumerate(descriptors):
         if i == 0:
             continue
 
-        matches = match_keypoints(descriptor, descriptors[i-1], distance_threthold, ratio_threthold)
+        matches = match_keypoints(
+            descriptor, descriptors[i - 1], distance_threthold, ratio_threthold
+        )
         if cross_check:
             tmp_matches = matches.copy()
-            matches_inverse = match_keypoints(descriptors[i-1], descriptor, distance_threthold, ratio_threthold)
+            matches_inverse = match_keypoints(
+                descriptors[i - 1], descriptor, distance_threthold, ratio_threthold
+            )
 
             for index1, index2 in matches:
                 if (index2, index1) not in matches_inverse:
@@ -73,7 +91,9 @@ def match_all_images(descriptors: List[np.ndarray], distance_threthold: float, r
     return all_matches
 
 
-def calc_homography(keypoints: List[cv2.KeyPoint], matches: List[Dict[Tuple[int, int], float]]) -> np.ndarray:
+def calc_homography(
+    keypoints: List[cv2.KeyPoint], matches: List[Dict[Tuple[int, int], float]]
+) -> np.ndarray:
     homographies = np.empty((0, 9))
 
     for i, match in enumerate(matches):
@@ -87,10 +107,30 @@ def calc_homography(keypoints: List[cv2.KeyPoint], matches: List[Dict[Tuple[int,
             destination = np.hstack((destination, destination_keypoint.pt))
             source_x, source_y = source_keypoint.pt
             destination_x, destination_y = destination_keypoint.pt
-            coefficient_tmp = np.array([
-                [source_x, source_y, 1, 0, 0, 0, -source_x * destination_x, -source_y * destination_x],
-                [0, 0, 0, source_x, source_y, 1, -source_x * destination_y, -source_y * destination_y]
-            ])
+            coefficient_tmp = np.array(
+                [
+                    [
+                        source_x,
+                        source_y,
+                        1,
+                        0,
+                        0,
+                        0,
+                        -source_x * destination_x,
+                        -source_y * destination_x,
+                    ],
+                    [
+                        0,
+                        0,
+                        0,
+                        source_x,
+                        source_y,
+                        1,
+                        -source_x * destination_y,
+                        -source_y * destination_y,
+                    ],
+                ]
+            )
             coefficient = np.vstack((coefficient, coefficient_tmp))
 
         homography = np.linalg.pinv(coefficient) @ destination
@@ -100,14 +140,22 @@ def calc_homography(keypoints: List[cv2.KeyPoint], matches: List[Dict[Tuple[int,
     return homographies
 
 
-def stitch_images(images: List[np.ndarray], homographies: np.ndarray, empty_image: np.ndarray, shift: Tuple[int, int], base:int = 0):
+def stitch_images(
+    images: List[np.ndarray],
+    homographies: np.ndarray,
+    empty_image: np.ndarray,
+    shift: Tuple[int, int],
+    base: int = 0,
+):
     base_image = images[base]
     base_height, base_width, _ = base_image.shape
     stitched_image = empty_image.copy()
 
-    stitched_image[shift[1]:base_height + shift[1], shift[0]:base_width + shift[0]] = base_image
+    stitched_image[
+        shift[1] : base_height + shift[1], shift[0] : base_width + shift[0]
+    ] = base_image
 
-    for image, homography in zip(images[:base] + images[base+1:], homographies):
+    for image, homography in zip(images[:base] + images[base + 1 :], homographies):
         homography = homography.reshape(3, 3)
         inv_homography = np.linalg.inv(homography)
         min_x, min_y, max_x, max_y = calc_min_max_coordinate(image, homography)
@@ -118,7 +166,9 @@ def stitch_images(images: List[np.ndarray], homographies: np.ndarray, empty_imag
                 source_x, source_y = source_coord[:-1].clip(0).astype(np.int64)
                 assert 0 <= source_x and 0 <= source_y
 
-                stitched_image[y + shift[1], x + shift[0]] = image[source_y, source_x, :]
+                stitched_image[y + shift[1], x + shift[0]] = image[
+                    source_y, source_x, :
+                ]
 
     return stitched_image.clip(0, 255).astype(np.uint8)
 
@@ -126,7 +176,9 @@ def stitch_images(images: List[np.ndarray], homographies: np.ndarray, empty_imag
 def main(args: argparse.Namespace) -> None:
     images = load_images(args.images)
     keypoints, descriptors = detect_keypoints(images)
-    matches = match_all_images(descriptors, args.distance_threthold, args.ratio_threthold, args.cross_check)
+    matches = match_all_images(
+        descriptors, args.distance_threthold, args.ratio_threthold, args.cross_check
+    )
     homographies = calc_homography(keypoints, matches)
 
     composited_homographies = composite_homographies(homographies)
@@ -134,19 +186,33 @@ def main(args: argparse.Namespace) -> None:
     empty_image = np.zeros((height, width, 3))
 
     stitched_image = stitch_images(images, composited_homographies, empty_image, shift)
-    cv2.imshow('img_stitched', stitched_image)
+    cv2.imshow("img_stitched", stitched_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--images', help='input images', nargs='+', type=str, required=True)
+    parser.add_argument(
+        "-i", "--images", help="input images", nargs="+", type=str, required=True
+    )
 
-    parser.add_argument('-d', '--distance_threthold', help='threshold of L2 distance for matching', type=float, default=10)
-    parser.add_argument('-r', '--ratio_threthold', help='threshold of the ratio of the first to the second in matching', type=float, default=0.8)
-    parser.add_argument('-c', '--cross_check', action='store_true')
+    parser.add_argument(
+        "-d",
+        "--distance_threthold",
+        help="threshold of L2 distance for matching",
+        type=float,
+        default=10,
+    )
+    parser.add_argument(
+        "-r",
+        "--ratio_threthold",
+        help="threshold of the ratio of the first to the second in matching",
+        type=float,
+        default=0.8,
+    )
+    parser.add_argument("-c", "--cross_check", action="store_true")
 
     args = parser.parse_args()
 
